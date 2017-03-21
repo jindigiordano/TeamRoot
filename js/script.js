1d88dc646abe028aa002c0ade497ac1d10474495
Jin Di Giordano <jin.di.giordano@gmail.com>
2017-03-21 13:25:28 -0700

    Add scroll animation

--- js/script.js
+++ js/script.js
@@ -3,15 +3,31 @@ $(document).ready(function(){
   $("#did-u-kno").hide();
   $("#resources").hide();
 
-  $("header a").on("click", function(){
+  $("header a").on("click", function(e){
+    e.preventDefault();
     $("main").show();
+    var target= $("main");
+    $('html, body').stop().animate({
+      scrollTop: target.offset().top
+    }, 1000);
   });
 
-  $("#movie").on("click", function(){
+  $("#movie").on("click", function(e){
+    e.preventDefault();
     $("#did-u-kno").show();
+    var target= $("#did-u-kno");
+    $('html, body').stop().animate({
+      scrollTop: target.offset().top
+    }, 1000);
   });
 
-  $("#did-u-kno a").on("click", function(){
+  $("#did-u-kno a").on("click", function(e){
+    e.preventDefault();
     $("#resources").show();
+    var target= $("#resources");
+    $('html, body').stop().animate({
+      scrollTop: target.offset().top
+    }, 1000);
   });
+  
 });
